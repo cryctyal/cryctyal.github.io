2e95bfcb62c8907f1f2c059b8d687d4f97fb7dc8
cryctyal <296833510@qq.com>
2018-05-14 14:21:42 +0800

    Site updated: 2018-05-14 14:21:42

--- js/script.js
+++ js/script.js
@@ -38,60 +38,42 @@
     e.stopPropagation();
 
     var $this = $(this),
-      type = $this.attr('data-share'),
+      url = $this.attr('data-url'),
+      encodedUrl = encodeURIComponent(url),
+      id = 'article-share-box-' + $this.attr('data-id'),
       offset = $this.offset();
 
-    if (type == 'baidu') {
-      var box = $('#article-share-box');
+    if ($('#' + id).length){
+      var box = $('#' + id);
 
       if (box.hasClass('on')){
         box.removeClass('on');
         return;
       }
+    } else {
+      var html = [
+        '<div id="' + id + '" class="article-share-box">',
+          '<input class="article-share-input" value="' + url + '">',
+          '<div class="article-share-links">',
+            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+          '</div>',
+        '</div>'
+      ].join('');
 
-      $('.article-share-box.on').hide();
+      var box = $(html);
 
-      box.css({
-        top: offset.top + 25,
-        left: offset.left - 25
-      }).addClass('on');
-    } else{
-      var url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id');
+      $('body').append(box);
+    }
 
-      if ($('#' + id).length){
-        var box = $('#' + id);
+    $('.article-share-box.on').hide();
 
-        if (box.hasClass('on')){
-          box.removeClass('on');
-          return;
-        }
-      } else {
-        var html = [
-          '<div id="' + id + '" class="article-share-box">',
-            '<input class="article-share-input" value="' + url + '">',
-            '<div class="article-share-links">',
-              '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-              '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-              '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-              '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-            '</div>',
-          '</div>'
-        ].join('');
-
-        var box = $(html);
-
-        $('body').append(box);
-      }
-
-      $('.article-share-box.on').hide();
-
-      box.css({
-        top: offset.top + 25,
-        left: offset.left
-      }).addClass('on');
-    };
+    box.css({
+      top: offset.top + 25,
+      left: offset.left
+    }).addClass('on');
   }).on('click', '.article-share-box', function(e){
     e.stopPropagation();
   }).on('click', '.article-share-box-input', function(){
@@ -140,35 +122,16 @@
   }
 
   $('#main-nav-toggle').on('click', function(){
-    $(".site-nav").fadeToggle(100);
+    if (isMobileNavAnim) return;
+
+    startMobileNavAnim();
+    $container.toggleClass('mobile-nav-on');
+    stopMobileNavAnim();
   });
 
   $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('site-nav')) return;
+    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
 
-    $container.removeClass('site-nav');
+    $container.removeClass('mobile-nav-on');
   });
-
-  $(window).scroll(function(){
-
-      var scrollTop = $(window).scrollTop();
-      if ( scrollTop >200 ){
-          $("#title-nav").show("fast");
-      } else{
-          $("#title-nav").hide("fast");
-      }
-
-  });
-
-  $(document).keydown(function(e){
-    if (e.keyCode == 37) {
-       $("#article-nav-newer")[0].click();
-       return false;
-    }
-
-    if (e.keyCode == 39) {
-       $("#article-nav-older")[0].click();
-       return false;
-    }
-});
 })(jQuery);
